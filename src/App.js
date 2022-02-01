aad7747036c784ca5ddb8343087234420605af05
Carlos Plata <cplata@xpediantsolutions.com>
2022-01-31 19:37:23 -0600

    Updated Algolia Search Demo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import algoliasearch from 'algoliasearch/lite';
+import React from 'react';
+import {
+	InstantSearch,
+	Hits,
+	SearchBox,
+	Pagination,
+	ClearRefinements,
+	RefinementList,
+	Configure,
+} from 'react-instantsearch-dom';
+import Hit from './Hit';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const searchClient = algoliasearch(
+	'B1G2GM9NG0',
+	'aadef574be1f9252bb48d4ea09b5cfe5'
+);
+
+const App = () => {
+	return (
+		<div className="ais-InstantSearch">
+			<h1>Algolia Search demo</h1>
+			<InstantSearch indexName="demo_ecommerce" searchClient={searchClient}>
+				<div className="left-panel">
+					<ClearRefinements />
+					<h2>Brands</h2>
+					<RefinementList attribute="brand" />
+					<Configure hitsPerPage={8} />
+				</div>
+				<div className="right-panel">
+					<SearchBox />
+					<Hits hitComponent={Hit} />
+					<Pagination />
+				</div>
+			</InstantSearch>
+		</div>
+	);
 }
 
 export default App;
